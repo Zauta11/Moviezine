8dcb5fcbbdaba74a6705ccd60c65fec269c400b8
Ivane Zautashvili <IvaneZautashvili11@gmail.com>
2022-10-30 14:49:09 +0400

    new

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -8,13 +8,4 @@ export class AuthService {
 
   constructor() { }
 
-
-
-  login(uname: string, pass: string) {
-    if(uname === "Giorgi" && pass === "1234") {
-      return 200
-    }else{
-      return 403;
-    }
-  }
 }
